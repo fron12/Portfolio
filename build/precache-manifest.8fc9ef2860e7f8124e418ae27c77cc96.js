533d73ca426d9fae935dc51eca10d82215968819
Fred <sohnf90@yahoo.com>
2019-03-29 01:20:30 -0700

    remove build from gitignore

---
+++ build/precache-manifest.8fc9ef2860e7f8124e418ae27c77cc96.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e88312cdd20ae8b9815e203c3dad0b86",
+    "url": "/Portfolio/static/media/blue-background.e88312cd.jpg"
+  },
+  {
+    "revision": "aa229599b07ad0243b2a",
+    "url": "/Portfolio/static/css/main.e9b97d32.chunk.css"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "ba5c19a38a678c8342ad",
+    "url": "/Portfolio/static/js/2.726ea24c.chunk.js"
+  },
+  {
+    "revision": "3f9d490937605fff654ade09e11eb748",
+    "url": "/Portfolio/static/media/Me.3f9d4909.jpg"
+  },
+  {
+    "revision": "aa229599b07ad0243b2a",
+    "url": "/Portfolio/static/js/main.9a63b1fd.chunk.js"
+  },
+  {
+    "revision": "749e7c534b797d590de2479f9e4d3c7f",
+    "url": "/Portfolio/static/media/Conway'sGameofLife.749e7c53.jpg"
+  },
+  {
+    "revision": "3a9faddadc32861dd9fe4b1496b01943",
+    "url": "/Portfolio/static/media/Don'tSendThatEmail.3a9fadda.jpg"
+  },
+  {
+    "revision": "f6233c433e42a8ef7224afedf9aba801",
+    "url": "/Portfolio/static/media/InstaClone.f6233c43.jpg"
+  },
+  {
+    "revision": "2b94b36518248d3037c211e5ade71821",
+    "url": "/Portfolio/static/media/LambdaNotes.2b94b365.jpg"
+  },
+  {
+    "revision": "359b64424812784245efaee106f21434",
+    "url": "/Portfolio/index.html"
+  }
+];
--- src/Todo.md
+++ src/Todo.md
@@ -1,4 +1,7 @@
 Improve intro.
 Add link to Lambda Notes.
 Deploy Lambda Notes.
-Add contact link for linkedin and tooltip for email.
+
+Insta-Clone:
+    Remove login page or implement login.
+    
