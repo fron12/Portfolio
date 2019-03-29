6b8100f05c002adbd4ab445bb0050e0383c2b6fc
Fred <sohnf90@yahoo.com>
2019-03-29 01:08:25 -0700

    Updates

---
+++ precache-manifest.7821b9ceb94bf09a2d3dd730c905083d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e88312cdd20ae8b9815e203c3dad0b86",
+    "url": "/Portfolio/static/media/blue-background.e88312cd.jpg"
+  },
+  {
+    "revision": "cb55da2f22ac304b781c",
+    "url": "/Portfolio/static/css/main.e9b97d32.chunk.css"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "9a4981d454673a5c09ab",
+    "url": "/Portfolio/static/js/2.4dc8f31c.chunk.js"
+  },
+  {
+    "revision": "3f9d490937605fff654ade09e11eb748",
+    "url": "/Portfolio/static/media/Me.3f9d4909.jpg"
+  },
+  {
+    "revision": "cb55da2f22ac304b781c",
+    "url": "/Portfolio/static/js/main.d7e35107.chunk.js"
+  },
+  {
+    "revision": "749e7c534b797d590de2479f9e4d3c7f",
+    "url": "/Portfolio/static/media/Conway'sGameofLife.749e7c53.jpg"
+  },
+  {
+    "revision": "3a9faddadc32861dd9fe4b1496b01943",
+    "url": "/Portfolio/static/media/Don'tSendThatEmail.3a9fadda.jpg"
+  },
+  {
+    "revision": "f6233c433e42a8ef7224afedf9aba801",
+    "url": "/Portfolio/static/media/InstaClone.f6233c43.jpg"
+  },
+  {
+    "revision": "2b94b36518248d3037c211e5ade71821",
+    "url": "/Portfolio/static/media/LambdaNotes.2b94b365.jpg"
+  },
+  {
+    "revision": "d6b12a6fe132c31fabff6d3f540d90b2",
+    "url": "/Portfolio/index.html"
+  }
+];
